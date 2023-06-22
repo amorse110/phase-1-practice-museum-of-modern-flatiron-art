39673f2dc9145b82951f358af612801ab5a58dd3
Austin Morse <amorse110@gmail.com>
2023-06-22 12:08:52 -0700

    Working on 3rd deliverable

--- src/index.js
+++ src/index.js
@@ -5,19 +5,24 @@ const exImage = document.getElementById('exhibit-image')
 const exComments = document.getElementById('comments-section')
 const commentButton = document.getElementById('comment-form')
 let comArray = [];
+const ticketButton = document.getElementById('buy-tickets-button')
+const boughtTickets = document.getElementById('tickets-bought')
 
 //EVENT LISTENERS
 commentButton.addEventListener('submit', addComment)
+ticketButton.addEventListener('click', buyTicket)
 
 //FETCH
 fetch(`${exhibitAPI}/1`)
 .then(res => res.json())
 .then(renderFirstExhibit)
 
+//Deliverable 1
 function renderFirstExhibit(exhibit) {
   exTitle.textContent = exhibit.title;
   exDescription.textContent = exhibit.description;
   exImage.src = exhibit.image;
+  boughtTickets.textContent = `${exhibit.tickets_bought} Tickets Bought`;
   comArray = exhibit.comments;
   let arrayLength = comArray.length;
   let comment
@@ -28,7 +33,7 @@ function renderFirstExhibit(exhibit) {
   }
 }
 
-//Deliverable 2 WORK IN PROGRESS
+//Deliverable 2
 function addComment(event) {
   event.preventDefault();
   const form = event.target;
@@ -39,7 +44,11 @@ function addComment(event) {
   comArray.push(newComment)
 }
 
-
+//Deliverable 3 Work In Progress
+function buyTicket(exhibit) {
+  boughtTickets.value = `${parseInt(boughtTickets.value) + 1} Tickets Bought`;
+  console.log(boughtTickets)
+}
 
 
 //MANTRA
