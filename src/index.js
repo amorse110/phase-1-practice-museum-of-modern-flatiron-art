f4f9bb7a639b2b6cf89e9d886c5bc30815de2d33
Austin Morse <amorse110@gmail.com>
2023-06-22 11:26:49 -0700

    first deliverable done, having trouble with second

--- src/index.js
+++ src/index.js
@@ -1,1 +1,50 @@
-console.log('Write your code here');
+const exhibitAPI = "http://localhost:3000/current-exhibits"
+const exTitle = document.getElementById('exhibit-title')
+const exDescription = document.getElementById('exhibit-description')
+const exImage = document.getElementById('exhibit-image')
+const exComments = document.getElementById('comments-section')
+const commentButton = document.getElementById('comment-form')
+let comArray = [];
+
+
+//EVENT LISTENERS
+commentButton.addEventListener('submit', addComment)
+
+
+
+//FETCH
+fetch(`${exhibitAPI}/1`)
+.then(res => res.json())
+.then(renderFirstExhibit)
+
+function renderFirstExhibit(exhibit) {
+  exTitle.textContent = exhibit.title;
+  exDescription.textContent = exhibit.description;
+  exImage.src = exhibit.image;
+  let comArray = exhibit.comments;
+  let arrayLength = comArray.length;
+  let comment
+  for (i = 0; i < arrayLength; i++) {
+    comment = document.createElement('p');
+    comment.textContent = comArray[i];
+    exComments.appendChild(comment);
+  }
+}
+
+
+function addComment(event) {
+  event.preventDefault();
+  const form = event.target;
+  const newComment = form.comment.value
+  comArray.push(newComment)
+}
+
+
+
+
+//MANTRA
+// FETCH the data
+// SELECT DOM elements
+// CREATE new elements (if needed)
+// ASSIGN data to elements
+// APPEND elements into DOM
